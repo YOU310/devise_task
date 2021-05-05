8ba1ca754fc24f3e09ac8803a8323b6bae984f3e
YOU <youmaya310@gmail.com>
2021-05-05 11:35:18 +0900

    マイページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
   # get 'games/index'
   # get 'games/new'
   # get 'games/creare'
   root to: "games#index"
   devise_for :users
   resources :games
+  resource :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
