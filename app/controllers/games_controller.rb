7a4cee9fbbdde4994880ffdb109c8394587c9084
YOU <youmaya310@gmail.com>
2021-05-05 11:25:31 +0900

    新規登録実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,21 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @games = Game.all
   end
 
   def new
+    @game = Game.new
   end
 
-  def creare
+  def create
+    Game.create!(name: game_params[:name], genre: game_params[:genre], user_id: current_user.id)
+    redirect_to root_path
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:name, :genre)
   end
 end
